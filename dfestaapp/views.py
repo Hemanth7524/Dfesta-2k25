d8db4ab91ea553b0814e144255e479e50d6b6a61
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2025-04-08 12:39:24 +0530

    Events Updated

--- dfestaapp/views.py
+++ dfestaapp/views.py
@@ -21,6 +21,39 @@ def registrations(request):
 def info(request):
     return render(request,'info.html')
 
+def event(request):
+    event_name = request.GET.get('event')
+    event = {}
+    if event_name == "bughunt":
+        event = {"name": "Bug Hunt", "description": "A fun event to find bugs in the code!", "img":"bughunt.jpeg"}
+    if event_name == "cinelens":
+        event = {"name": "Cine Lens", "description": "A film-making competition!", "img":"cinelens.jpeg"}
+    if event_name == "datapirates":
+        event = {"name": "Data Pirates", "description": "A data analysis competition!", "img":"datapirates.jpeg"}
+    if event_name == "E-Sports":
+        event = {"name": "E-Sports", "description": "A gaming competition!", "img":"esports.jpeg"}
+    if event_name == "f2":
+        event = {"name": "F2", "description": "A Formula 2 racing event!", "img":"f2.jpeg"}
+    if event_name == "Fun-Activities":
+        event = {"name": "Fun Factory", "description": "A fun event with games and activities!", "img":"funfactory.jpeg"}
+    if event_name == "mindmaze":
+        event = {"name": "Mind Maze", "description": "A puzzle-solving competition!", "img":"mindmaze.jpeg"}
+    if event_name == "paperpresentation":
+        event = {"name": "Paper Presentation", "description": "A competition to present research papers!", "img":"paperpresentation.jpeg"}
+    if event_name == "projectexpo":
+        event = {"name": "Project Expo", "description": "An exhibition of innovative projects!", "img":"projectexpo.jpeg"}
+    if event_name == "squidgame":
+        event = {"name": "Squid Game", "description": "A fun game event!", "img":"squidgame.jpeg"}
+    if event_name == "Quiz":
+        event = {"name": "Tech Quiz", "description": "A quiz competition on technology!", "img":"techquiz.jpeg"}
+        event = {"name": "Tech Quiz", "description": "A quiz competition on technology!", "img":"techquiz.jpeg"}
+    if event_name == "visionverse":
+        event = {"name": "Vision Verse", "description": "A competition to showcase your vision!", "img":"visionverse.jpeg"}
+    if event_name == "web":
+        event = {"name": "Web Xpress", "description": "A web development competition!", "img":"webxpress.jpeg"}
+
+    return render(request,'event_detail.html', {'event':event})
+
 
 def register(request):
     eventname = request.GET.get('event')
